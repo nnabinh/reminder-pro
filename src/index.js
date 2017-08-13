217e257e251d93e985a154eda8771d3e6b66d833
Binh Nguyen <nnabinh@gmail.com>
2017-08-14 01:51:59 +0900

    work with redux - create very first actions & reducers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App'
+import reducer from './reducers'
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App/>
+    </Provider>, document.getElementById('root')
+);
